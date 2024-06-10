7a4239163ab90661ff220d828cb655b58c5570d1
Alex Caza <alex@alexcaza.com>
2024-06-10 18:14:40 -0400

    Add constants for links and collections

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,16 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Alex Caza';
+export const SITE_DESCRIPTION = 'Product engineer, photographer, writer.';
+
+// TODO: There's probably a better way to make collections typesafe
+export enum Collections {
+  blog = 'personal'  
+};
+
+// TODO: There's probably a better way to make URLs typesafe or genertic
+export const mkBlogLink = (slug?: string): string => slug ? `/personal/${slug}` : '/personal';
+export const mkHomeLink = (): string => '/';
+export const mkResumeLink = (): string => '/resume';
+export const mkNowLink = (): string => '/now';
