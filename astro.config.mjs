1842725b3f48cb9ff0936f012405492f749cd502
Alex Caza <alex@alexcaza.com>
2024-06-21 20:31:54 -0400

    Revert "Temp change site and base in config"
    This reverts commit d309e7c0d2555673c85d6440ad49ed2a15bf9f73.

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://alexcaza.github.io',
-	base: 'alexcaza.com',
+	site: 'https://alexcaza.com',
 	integrations: [mdx(), sitemap()],
 	markdown: {
 		shikiConfig: {
